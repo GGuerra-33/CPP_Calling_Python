221b4e1c673a19e7e55d4e94cd1385836a6edb45
Guillermo Guerra <guiguri09@gmail.com>
2023-02-19 20:59:34 +0100

    A simple example of a language calling other language

---
+++ read_numbers.py
@@ -0,0 +1,9 @@
+from sys import stdin
+
+def main():
+    for num in sorted(map(int, next(stdin).split(" ")[:-1])):
+        print(num, end=" ")
+    print()
+
+if __name__ == "__main__":
+    main()
